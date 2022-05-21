5e13b4c04ffaff6ecc0a9ff93694324cfd4bff0e
suhaila <suhailasalam47@gmail.com>
2022-05-21 22:32:23 +0530

    variation manager for variation model

--- cart/views.py
+++ cart/views.py
@@ -13,9 +13,11 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    color = request.GET['color']
-    size = request.GET['size']
-    return HttpResponse(color + " " + size)
+    if request.method == 'POST':
+        color = request.POST['color']
+        size = request.POST['size']
+        print(color, size)
+
     # get the product
     product = Product.objects.get(id=product_id)
     try:
