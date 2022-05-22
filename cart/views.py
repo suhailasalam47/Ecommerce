6606ab2b3f587422345d867401d9f04c31299a13
suhaila <suhailasalam47@gmail.com>
2022-05-22 10:48:33 +0530

    variation added to CartItem model

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponse
 
 
 def _cart_id(request):
@@ -13,13 +12,25 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    if request.method == 'POST':
-        color = request.POST['color']
-        size = request.POST['size']
-        print(color, size)
-
     # get the product
     product = Product.objects.get(id=product_id)
+    product_variation = []
+
+    if request.method == 'POST':
+        for item in request.POST :
+            key = item
+            value = request.POST[key]
+            
+            try:
+                variation = Variation.objects.get(
+                    product = product,
+                    variation_category__iexact=key,
+                    variation_value__iexact=value,
+                )
+                product_variation.append(variation)
+            except:
+                pass    
+
     try:
         # get the cart using the cart id present in the session
         cart = Cart.objects.get(cart_id=_cart_id(request))
