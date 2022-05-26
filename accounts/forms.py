66ec5ab69c9c2091150c8985ad5c65dcb5f9f86e
suhaila <suhailasalam47@gmail.com>
2022-05-26 16:23:16 +0530

    email,password validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -32,3 +32,14 @@ class RegistrationForm(forms.ModelForm):
         ] = "Enter your phone number"
         for field in self.fields:
             self.fields[field].widget.attrs["class"] = "form-control"
+ 
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password!=confirm_password:
+            raise forms.ValidationError(
+                "password not matching"
+            )
+        
