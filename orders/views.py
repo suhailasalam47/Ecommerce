033fc9c9a7e43476cbe15af6206e2944da2e45a5
suhaila <suhailasalam47@gmail.com>
2022-06-25 18:36:25 +0530

    Order review page

--- orders/views.py
+++ orders/views.py
@@ -56,7 +56,15 @@ def place_order(request, total=0, quantity=0):
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
-            messages.success(request, "details saved successfuly")
-            return redirect('checkout')
+            
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order' : order,
+                'total' : total,
+                'grand_total' : grand_total,
+                'tax' : tax,
+                'cart_items' : cart_items,
+            }
+            return render(request, 'orders/payments.html', context)
         else:
             return redirect('checkout')
