30495801c215f1ef0f32d6274cbad08dff9410e9
suhaila <suhailasalam47@gmail.com>
2022-06-25 16:01:47 +0530

    Payment template

--- orders/views.py
+++ orders/views.py
@@ -6,7 +6,10 @@ from .forms import OrderForm
 from django.contrib import messages
 
 # Create your views here.
+def payments(requset):
+    return render(requset, 'orders/payments.html')
 
+    
 def place_order(request, total=0, quantity=0):
     current_user = request.user
     cart_items = CartItem.objects.filter(user=current_user)
