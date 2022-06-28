c36fc63d697805602fffba39c83586b794ed31cc
suhaila <suhailasalam47@gmail.com>
2022-06-28 12:12:59 +0530

    showing orderProduct details along with order in backend

--- orders/admin.py
+++ orders/admin.py
@@ -3,11 +3,31 @@ from .models import Order, Payment, OrderProduct
 # Register your models here.
 
 
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    extra = 0
+    readonly_fields = ('payment',
+                        'user', 
+                        'product', 
+                        'variation', 
+                        'quantity',
+                        'product_price',
+                        'ordered'
+                        )
+
+
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['order_number','first_name','phone','email','status','is_ordered']
+    list_display = ['order_number',
+                    'first_name',
+                    'phone',
+                    'email',
+                    'status',
+                    'is_ordered'
+                    ]
     list_filter = ['is_ordered', 'status']
     search_fields = ['first_name','phone','email','order_number']
     list_per_page = 22
+    inlines = [OrderProductInline]
 
 admin.site.register(Payment)
 admin.site.register(Order,OrderAdmin)
