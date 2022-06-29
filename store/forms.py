d22d1c260bcace59cf5a50816248f8ecc258136f
suhaila <suhailasalam47@gmail.com>
2022-06-29 21:52:36 +0530

    rating stars

---
+++ store/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import ReviewRating
+
+
+class ReviewRatingForm(forms.ModelForm):
+    class Meta:
+        model = ReviewRating
+        fields = ['subject','review','rating']
