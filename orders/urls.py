a5555cbc21fc499c3dd64c9dc0a9cfd9f0c2c738
suhaila <suhailasalam47@gmail.com>
2022-07-26 09:33:01 +0530

    Button for Cash on delivery added

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("place_order/", views.place_order, name="place_order"),
     path("payments/", views.payments, name="payments"),
     path("order_success/", views.order_success, name="order_success"),
+    path('cash_on_delivery/', views.cash_on_delivery, name="cash_on_delivery"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -10,6 +10,61 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.http import JsonResponse
 
+# import razorpay
+# from django.conf import settings
+# from django.views.decorators.csrf import csrf_exempt
+# from django.http import HttpResponseBadRequest
+ 
+ 
+# # authorize razorpay client with API Keys.
+# razorpay_client = razorpay.Client(
+#     auth=(settings.RAZOR_KEY_ID, settings.RAZOR_KEY_SECRET))
+
+
+# @csrf_exempt
+# def paymenthandler(request):
+ 
+#     # only accept POST request.
+#     if request.method == "POST":
+#         try:
+           
+#             # get the required parameters from post request.
+#             payment_id = request.POST.get('razorpay_payment_id', '')
+#             razorpay_order_id = request.POST.get('razorpay_order_id', '')
+#             signature = request.POST.get('razorpay_signature', '')
+#             params_dict = {
+#                 'razorpay_order_id': razorpay_order_id,
+#                 'razorpay_payment_id': payment_id,
+#                 'razorpay_signature': signature
+#             }
+ 
+#             # verify the payment signature.
+#             result = razorpay_client.utility.verify_payment_signature(
+#                 params_dict)
+#             if result is None:
+#                 amount = 20000  # Rs. 200
+#                 try:
+ 
+#                     # capture the payemt
+#                     razorpay_client.payment.capture(payment_id, amount)
+ 
+#                     # render success page on successful caputre of payment
+#                     return render(request, 'paymentsuccess.html')
+#                 except:
+ 
+#                     # if there is an error while capturing payment.
+#                     return render(request, 'paymentfail.html')
+#             else:
+ 
+#                 # if signature verification fails.
+#                 return render(request, 'paymentfail.html')
+#         except:
+ 
+#             # if we don't find the required parameters in POST data
+#             return HttpResponseBadRequest()
+#     else:
+#        # if other than POST request is made.
+#         return HttpResponseBadRequest()
 
 # Create your views here.
 def payments(requset):
@@ -168,3 +223,7 @@ def order_success(requset):
         return render(requset, "orders/order_success.html", context)
     except(Payment.DoesNotExist, Order.DoesNotExist):
         return redirect('home')
+
+
+def cash_on_delivery(requset):
+    return render(requset, "orders/order_success.html")
