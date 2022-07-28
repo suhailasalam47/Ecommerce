ef37e2343dfe3955280a836a3483d981b98448aa
suhaila <suhailasalam47@gmail.com>
2022-07-28 09:23:50 +0530

    payment object created for COD

--- orders/views.py
+++ orders/views.py
@@ -10,63 +10,7 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.http import JsonResponse
 
-# import razorpay
-# from django.conf import settings
-# from django.views.decorators.csrf import csrf_exempt
-# from django.http import HttpResponseBadRequest
- 
- 
-# # authorize razorpay client with API Keys.
-# razorpay_client = razorpay.Client(
-#     auth=(settings.RAZOR_KEY_ID, settings.RAZOR_KEY_SECRET))
 
-
-# @csrf_exempt
-# def paymenthandler(request):
- 
-#     # only accept POST request.
-#     if request.method == "POST":
-#         try:
-           
-#             # get the required parameters from post request.
-#             payment_id = request.POST.get('razorpay_payment_id', '')
-#             razorpay_order_id = request.POST.get('razorpay_order_id', '')
-#             signature = request.POST.get('razorpay_signature', '')
-#             params_dict = {
-#                 'razorpay_order_id': razorpay_order_id,
-#                 'razorpay_payment_id': payment_id,
-#                 'razorpay_signature': signature
-#             }
- 
-#             # verify the payment signature.
-#             result = razorpay_client.utility.verify_payment_signature(
-#                 params_dict)
-#             if result is None:
-#                 amount = 20000  # Rs. 200
-#                 try:
- 
-#                     # capture the payemt
-#                     razorpay_client.payment.capture(payment_id, amount)
- 
-#                     # render success page on successful caputre of payment
-#                     return render(request, 'paymentsuccess.html')
-#                 except:
- 
-#                     # if there is an error while capturing payment.
-#                     return render(request, 'paymentfail.html')
-#             else:
- 
-#                 # if signature verification fails.
-#                 return render(request, 'paymentfail.html')
-#         except:
- 
-#             # if we don't find the required parameters in POST data
-#             return HttpResponseBadRequest()
-#     else:
-#        # if other than POST request is made.
-#         return HttpResponseBadRequest()
-
-# Create your views here.
 def payments(requset):
     body = json.loads(requset.body)
     order = Order.objects.get(
@@ -187,6 +131,10 @@ def place_order(request, total=0, quantity=0):
                 is_ordered=False,
                 order_number=order_number,
             )
+            if order:
+                print("inside if--------",order.order_number)
+                request.session["order_id"]= order_number
+
             context = {
                 "order": order,
                 "total": total,
@@ -226,4 +174,28 @@ def order_success(requset):
 
 
 def cash_on_delivery(requset):
+    if requset.method == 'POST':
+        try:
+            order_id = requset.session["order_id"]
+        except:
+            pass
+        order = Order.objects.get(
+            user=requset.user, 
+            is_ordered=False,
+            order_number=order_id
+        )
+        user= requset.user
+
+        payment = Payment(
+            user=user,
+            Payment_id=order_id,
+            Payment_method="Cash on delivery",
+            amount_paid=order.order_total,
+            status="Ordered",
+        )
+        payment.save()
+
+        order.Payment = payment
+        order.is_ordered = True
+        order.save()    
     return render(requset, "orders/order_success.html")
