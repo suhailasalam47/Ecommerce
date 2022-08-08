774f994aaec1dd123c7ec79969f9adf88e32ae07
suhaila <suhailasalam47@gmail.com>
2022-08-08 10:31:27 +0530

    Rating star displaying on Home page

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, ReviewRating
 
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True)
+    products = Product.objects.all().filter(is_available=True).order_by('-created_date')
+    for i in products:
+        review = ReviewRating.objects.filter(product_id=i.id, status=True)
 
     context = {
         "products": products,
+        'review' : review,
     }
     return render(request, "home.html", context)
